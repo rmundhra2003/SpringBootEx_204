865c9b7d90341e6147321cb6c7fe8ab4562361eb
Rekha Mundhra <rmundhra2003@yahoo.com>
2019-05-14 15:40:55 -0400

    initial commit

---
+++ src/main/java/com/example/springboot_ex204/SpringbootEx204Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_ex204;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEx204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEx204Application.class, args);
+	}
+
+}
